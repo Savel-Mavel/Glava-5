3261ddd40ee8c5915f7d68eac97fac4b2f0ece8f
Savel-Mavel <savelxgy279@mail.ru>
2026-09-22 22:47:03 +0700

    Урок 6 за

---
+++ test_lesson06_task1.py
@@ -0,0 +1,22 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def test_dynamic_loading():
+    driver = webdriver.Chrome()
+    wait = WebDriverWait(driver, 20)
+
+    driver.get("https://the-internet.herokuapp.com/dynamic_loading/2")
+
+    start_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div#start button")))
+    start_btn.click()
+
+    finish_msg = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div#finish h4")))
+
+    driver.save_screenshot("dynamic_loading_result.png")
+
+    assert finish_msg.text == "Hello World!", f"Ожидался 'Hello World!', но получен '{finish_msg.text}'"
+
+    driver.quit()
